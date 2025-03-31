d74e854c87c5ba044057fafa3b0065f995c4ef5f
DK Mehta <dineshmehta@DKs-Mac.local>
2025-03-31 06:37:58 +0530

    fist push

---
+++ questions/pusingZeroBack.js
@@ -0,0 +1,21 @@
+
+
+function moveZeroes(nums) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            count++;
+        } else {
+            nums[i - count] = nums[i];
+        }
+    }
+    for (let i = nums.length - count; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+
+    return nums
+}
+
+
+
+console.log(moveZeroes([0, 1, 0, 3, 12]));
